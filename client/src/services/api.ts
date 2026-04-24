0ec04317fd86ebc9bf03148157ee327e8787e51b
Rajdeep <rajdeepsingha384@gmail.com>
2026-04-25 00:15:55 +0530

    fix : deployment issue

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,7 +4,7 @@ if (!import.meta.env.VITE_API_URL && import.meta.env.PROD) {
   throw new Error("[api.ts] VITE_API_URL is not set.");
 }
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
